280f2cd38bb198bbdeb69df6a4ea907dbab6fea5
anurag629 <akvermaav629@gmail.com>
2022-04-11 09:13:58 +0530

    category model created and working fine

--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("Hello i am here")
+    return JsonResponse({'info': 'Django React Course', 'name': "anurag"})
--- ecom/settings.py
+++ ecom/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
 
     'api',
+    'api.category',
 ]
 
 MIDDLEWARE = [
