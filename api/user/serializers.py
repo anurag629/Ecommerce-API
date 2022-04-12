45f74bfd7c8b6c048173c451b8d128194298fab5
anurag629 <akvermaav629@gmail.com>
2022-04-12 07:21:37 +0530

    done with user serializer

--- api/user/serializers.py
+++ api/user/serializers.py
@@ -16,6 +16,11 @@ class UserSerializer(serializers.hyperlinkedSerializer):
         return instance
 
     def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
 
         instance.save()
         return instance
