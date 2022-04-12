201e9a73f45ed514ea1fd386ed226f94095ac471
anurag629 <akvermaav629@gmail.com>
2022-04-12 08:09:26 +0530

    created function to generate tocken

--- api/user/views.py
+++ api/user/views.py
@@ -1,3 +1,9 @@
+from secrets import choice
 from django.shortcuts import render
 
+import random
 # Create your views here.
+
+
+def generate_session_token(length=10):
+    return ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)] + [str(i) for i in range(length)]) for _ in range(length))
