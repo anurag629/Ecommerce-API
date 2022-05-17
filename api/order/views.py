bfd62e22d90d019ca8bb458789de02cf6146427d
anurag629 <akvermaav629@gmail.com>
2022-05-17 11:59:13 +0530

    doing order

--- api/order/views.py
+++ api/order/views.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from rest_framework import viewsets
 from django.http import JsonResponse
 from django.contrib.auth import get_user_model
@@ -17,3 +18,35 @@ def validate_user_session(id, token):
         return False
     except UserModel.DoesNotExist:
         return False
+
+
+@csrf_exempt
+def add(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({'error': 'Please re-login', 'code': '1'})
+
+    if request.method == "POST":
+        user_id = id
+        transaction_id = request.POST['transaction_id']
+        amount = request.POST['amount']
+        products = request.POST['products']
+
+        total_pro = len(products.split('.')[:-1])
+
+        UserModel = get_user_model()
+
+        try:
+            user: UserModel.objects.get(pk=user_id)
+        except UserModel.DoesNotExist:
+            return JsonResponse({'error': 'User does not exist'})
+
+        ordr = Order(user=user, product_names=products, total_products=total_pro,
+                     transaction_id=transaction_id, total_amount=amount)
+        ordr.save()
+
+        return JsonResponse({'success': True, 'error': False, 'msg': 'Order placed successfully'})
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all().order_by('id')
+    seriazer_class = OrderSerilizer
